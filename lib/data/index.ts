393f952628cc9f61bf02969cb7a1a54fa8b7fb38
goldenstein64 <goldnstein64@gmail.com>
2024-10-18 17:27:20 -0400

    feat: index.ts re-exports "important" properties

---
+++ lib/data/index.ts
@@ -0,0 +1,5 @@
+export type { Mark } from "./Mark";
+export type { Message, Connection } from "./Messages";
+
+export { default as Board } from "./Board";
+export { marks } from "./Mark";
---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export type { Mark, Message, Connection } from "./data";
+
+export { Board, marks } from "./data";
+
+export { default as default } from "./Application";
+
+export { Human, EasyComputer, MediumComputer, HardComputer } from "./player";
