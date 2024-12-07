905e07c1db6223b0f2b92efaa8296f25a7da3692
goldenstein64 <goldnstein64@gmail.com>
2024-12-06 22:37:26 -0500

    feat: ConsoleConnection.prompt may throw EOFError

--- app/ConsoleConnection.ts
+++ app/ConsoleConnection.ts
@@ -1,11 +1,19 @@
 import type { Message, Connection } from "../lib/data/Messages";
 
+export class EOFError extends Error {
+  constructor(message: string = "EOF encountered!", options?: ErrorOptions) {
+    super(message, options);
+
+    this.name = "EOFError";
+  }
+}
+
 export default class ConsoleConnection implements Connection {
   constructor(readonly format: (message: Message) => string) {}
 
   async prompt(message: Message): Promise<string> {
     const input = prompt(this.format(message));
-    if (!input) throw new TypeError("EOF encountered!");
+    if (!input) throw new EOFError();
     return input;
   }
 
