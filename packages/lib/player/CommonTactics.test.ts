4b71773c6a56b3319df9ae9dfc0177c908c495b2
goldenstein64 <goldnstein64@gmail.com>
2025-03-04 02:04:56 -0500

    test: make commonTactics test getMoves, not getMove

--- packages/lib/player/CommonTactics.test.ts
+++ packages/lib/player/CommonTactics.test.ts
@@ -1,4 +1,4 @@
-import type { Player } from ".";
+import type Computer from "./Computer";
 import type { Mark } from "../data/Mark";
 
 import { describe, it, expect } from "bun:test";
@@ -7,19 +7,19 @@ import MediumComputer from "./MediumComputer";
 import HardComputer from "./HardComputer";
 import Board from "../data/Board";
 
-const computerCases: readonly (readonly [string, Player])[] = [
+const computerCases: readonly (readonly [string, Computer])[] = [
   ["MediumComputer", new MediumComputer()],
   ["HardComputer", new HardComputer()],
 ];
 
-const testCases: readonly (readonly [string, Mark, number])[] = [
-  [",XX,OO,,,", "X", 0], // X winning move
-  [",OO,XX,,X", "O", 0], // O winning move
-  ["O,,O,X,X,", "X", 6], // X blocking move
-  [",O,X,,XXO", "O", 0], // O blocking move
-  [",X,O,X,O,", "X", 2], // X trapping move
-  [",X,OXX,O,", "O", 6], // O trapping move
-  [",XXXOOOX,", "O", 0], // O edge case
+const testCases: readonly (readonly [string, Mark, number[]])[] = [
+  [",XX,OO,,,", "X", [0]], // X winning move
+  [",OO,XX,,X", "O", [0]], // O winning move
+  ["O,,O,X,X,", "X", [6]], // X blocking move
+  [",O,X,,XXO", "O", [0]], // O blocking move
+  [",X,O,X,O,", "X", [2]], // X trapping move
+  [",X,OXX,O,", "O", [6]], // O trapping move
+  [",XXXOOOX,", "O", [0]], // O edge case
 ];
 
 for (const [computerName, computer] of computerCases) {
@@ -28,7 +28,7 @@ for (const [computerName, computer] of computerCases) {
       it(`can solve ${pattern} as ${mark}`, async () => {
         const board = Board.fromPattern(pattern);
 
-        expect(await computer.getMove(board, mark)).toBe(expected);
+        expect(await computer.getMoves(board, mark)).toStrictEqual(expected);
       });
     }
   });
